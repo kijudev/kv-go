0476f79a5b0f24a1a61aeb57f393dc05d35ce73c
Kiju <dev@jakubkijek.com>
2026-09-25 19:37:30 +0200

    feat: Store interface

---
+++ kv/store.go
@@ -0,0 +1,13 @@
+// Package kv
+// The Store interface for different implementations.
+//
+// Note that all implementations of the Store interface provided
+// by this very library are in-memory, tested and thread-safe,
+// though I would not call them production-ready.
+package kv
+
+type Store interface {
+	Get(key string) ([]byte, bool)
+	Set(key string, value []byte)
+	Delete(key string) bool
+}
